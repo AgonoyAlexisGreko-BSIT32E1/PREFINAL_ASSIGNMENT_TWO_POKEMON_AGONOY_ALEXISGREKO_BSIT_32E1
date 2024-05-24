dc3983a8af725b5ff4341f7b2274c722fd173f01
AgonoyAlexisGreko-BSIT32E1 <148577459+AgonoyAlexisGreko-BSIT32E1@users.noreply.github.com>
2024-05-24 22:03:16 +0800

    update files

--- Prefinals_AgonoyAlexisGreko_Pokemon/Controllers/HomeController.cs
+++ Prefinals_AgonoyAlexisGreko_Pokemon/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Prefinals_AgonoyAlexisGreko_Pokemon.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
